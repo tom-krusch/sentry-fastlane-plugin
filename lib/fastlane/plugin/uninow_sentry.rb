75f917ffa0cc2beb8f2a41812b3d2ead9b301ac8
tom-krusch <tom.krusch@uninow.de>
2020-03-30 01:17:00 +0200

    wip

--- lib/fastlane/plugin/uninow_sentry.rb
+++ lib/fastlane/plugin/uninow_sentry.rb
@@ -1,7 +1,7 @@
 require 'fastlane/plugin/uninow_sentry/version'
 
 module Fastlane
-  module Sentry
+  module UninowSentry
     # Return all .rb files inside the "actions" and "helper" directory
     def self.all_classes
       Dir[File.expand_path('**/{actions,helper}/*.rb', File.dirname(__FILE__))]
